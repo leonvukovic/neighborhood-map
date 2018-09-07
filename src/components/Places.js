bb9449e7c404bb34e07e1fe74adb53db13cb0d46
Leon Vukovic <vukovic.leon@gmail.com>
2018-09-07 11:32:31 +0200

    feat: Place clicked data

--- src/components/Places.js
+++ src/components/Places.js
@@ -2,14 +2,14 @@ import React, { Component } from 'react'
 
 class Places extends Component {
   // Clicked places list item
-  openModal = () => {
-    console.log('jeje');
+  openModal = (venues) => {
+    console.log(venues.venue.name);
   }
 
   render() {
     // Map over data from state and make list of places
     const placesList = this.props.markers.map((venues, i) => {
-      return <li onClick={this.openModal} key={i}>{venues.venue.name}</li>
+      return <li onClick={(e) => this.openModal(venues, e)} key={i}>{venues.venue.name}</li>
     });
 
     return (
